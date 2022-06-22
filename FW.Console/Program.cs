6dea402a4e42a24bde6a716c1ffc6d38ceb661e1
ijlynivfhp <380726211@qq.com>
2022-06-22 23:01:34 +0800

    0000

--- FW.Console/Program.cs
+++ FW.Console/Program.cs
@@ -2,6 +2,8 @@
 using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,22 +16,60 @@ namespace FW.Console
         private static int TotalCount;
         static void Main(string[] args)
         {
-            int aaa = Environment.ProcessorCount;
-            int minWorker, minIOC;
-            // Get the current settings.
-            ThreadPool.GetMinThreads(out minWorker, out minIOC);
-            var tt = ThreadPool.SetMaxThreads(0, 0);
-            ThreadPool.GetMinThreads(out minWorker, out minIOC);
-            ThreadPool.GetAvailableThreads(out int workerCount,out int cpCount);
-            System.Console.WriteLine(workerCount); System.Console.WriteLine(cpCount);
-            
-            var aa= Task.Factory.StartNew(() => {
-                CommonHelpr.TestA();
-            });
-            var bb= Task.Factory.StartNew(() => {
-                CommonHelpr.TestB();
-            });
-            Task.WaitAll(new List<Task>(){ aa,bb }.ToArray());
+            var startDirectory = @"F:\";
+            var w = Stopwatch.StartNew();
+            ThisIsARecursiveFunction(startDirectory);
+            System.Console.WriteLine("Elapsed seconds: " + w.Elapsed.TotalSeconds);
+            System.Console.ReadKey();
+        }
+        public static void ThisIsARecursiveFunction(String currentDirectory)
+        {
+            var lastBit = Path.GetFileName(currentDirectory);
+            var depth = currentDirectory.Count(t => t == '\\');
+            //Console.WriteLine(depth + ": " + currentDirectory);
+            try
+            {
+                var children = Directory.GetDirectories(currentDirectory);
+                //Edit this mode to switch what way of parallelization it should use
+                int mode = 2;
+                switch (mode)
+                {
+                    case 1:
+                        foreach (var child in children)
+                        {
+                            ThisIsARecursiveFunction(child);
+                        }
+                        break;
+                    case 2:
+                        children.AsParallel().ForAll(t =>
+                        {
+                            ThisIsARecursiveFunction(t);
+                        });
+                        break;
+                    case 3:
+                        Parallel.ForEach(children, t =>
+                        {
+                            ThisIsARecursiveFunction(t);
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception eee)
+            {
+                //Exception might occur for directories that can't be accessed.
+            }
+
+
+
+            //var aa= Task.Factory.StartNew(() => {
+            //    CommonHelpr.TestA();
+            //});
+            //var bb= Task.Factory.StartNew(() => {
+            //    CommonHelpr.TestB();
+            //});
+            //Task.WaitAll(new List<Task>(){ aa,bb }.ToArray());
             ThreadPool.GetAvailableThreads(out int workerCount1, out int cpCount1);
             System.Console.WriteLine(1111111111);
             //System.Console.WriteLine($"abc{Thread.CurrentThread.ManagedThreadId}");
@@ -68,9 +108,6 @@ namespace FW.Console
             //new RabbitMqTopicSendHelper().SendMsg<dynamic>(new { aa = 11, bb = 22 }, FW.Common.Common.QueueNamePre + "." + 110, FW.Common.Common.QueueNamePre + "." + 110 + "." + 2222);
             //new RabbitMqTopicSendHelper().SendMsg<dynamic>(new { aa = 11, bb = 22 }, FW.Common.Common.QueueNamePre + "." + 220, FW.Common.Common.QueueNamePre + "." + 220 + "." + 1111);
             //new RabbitMqTopicSendHelper().SendMsg<dynamic>(new { aa = 11, bb = 22 }, FW.Common.Common.QueueNamePre + "." + 220, FW.Common.Common.QueueNamePre + "." + 220 + "." + 2222);
-            while (true) { 
-                System.Console.ReadLine();
-            }
         }
         static void DoJob()
         {
@@ -78,6 +115,13 @@ namespace FW.Console
             System.Console.WriteLine(TotalCount);
             while (true) {
             }
+        }
+
+        public static void Dosome()
+        {
+            System.Console.WriteLine($"Task Start ThreadId：{Thread.CurrentThread.ManagedThreadId}，DateTime：{DateTime.Now.ToLongTimeString()}");
+            Thread.Sleep(5 * 1000); // 模拟任务耗时
+            System.Console.WriteLine($"Task End ThreadId：{Thread.CurrentThread.ManagedThreadId}，DateTime：{DateTime.Now.ToLongTimeString()}");
         }
     }
 }
